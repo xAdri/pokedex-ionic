8ea0fc5a4a80978b2a8d58bc263e21da053a9be1
xAdri <aperezgfx@gmail.com>
2026-01-08 17:53:36 +0100

    Add fonts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonFooter } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonContent, IonButton, IonFooter, IonToolbar],
 })
 export class HomePage {
   constructor() {}
